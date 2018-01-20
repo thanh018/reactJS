f9d357f2c0dd7bddc9d64cdbbfaccbff90413126
thanh018 <lookat018@gmail.com>
2018-01-21 01:12:31 +0700

    form

--- src/App.js
+++ src/App.js
@@ -46,7 +46,15 @@ class App extends Component {
 
         //change-color-font
         color: 'red',
-        fontSize: 12
+        fontSize: 12,
+
+        //form
+        txtUsername: '',
+        txtPassword:'',
+        txtDescription:'',
+        sltGender: 1,
+        rdLang: 'en',
+        ckStatus: true
       }     
     }
 
@@ -85,6 +93,22 @@ class App extends Component {
     onClick(){
       console.log('This a icon block');
     }
+
+    onHandleChange=(event) => {
+      var target = event.target;
+      var name = target.name;
+
+      var value = (target.type === 'checkbox') ? target.checked : target.value;
+      this.setState({
+        [name]: value
+      });
+    }
+
+    onHandleSubmit=(event)=> {
+      event.preventDefault();
+      console.log(this.state);
+    }
+
   render() {
 
     // products
@@ -154,10 +178,126 @@ class App extends Component {
       return result;
     });
 
+
     return (
         <div className="render-wrapper">
           <div className="container">
             <Header />
+            
+            <div className="row">
+              <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                <div className="panel panel-primary">
+                  <div className="panel-heading">
+                    <h3 className="panel-title">Form</h3>
+                  </div>
+                  <div className="panel-body">
+                    
+                    <form onSubmit={this.onHandleSubmit}>
+                      <div className="form-group">
+                        <label>username</label>
+                        <input 
+                          type="text" 
+                          className="form-control" 
+                          name="txtUsername"
+                          value={this.state.txtUsername}
+                          onChange={this.onHandleChange}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label>Password</label>
+                        <input 
+                          type="password" 
+                          className="form-control" 
+                          name="txtPassword"
+                          value={this.state.txtPassword}
+                          onChange={this.onHandleChange}
+                        />
+                      </div>
+
+                      <div className="form-group">
+                        <label>Description</label>
+                        <textarea 
+                          type="text" 
+                          className="form-control" 
+                          name="txtDescription"
+                          rows="3"
+                          value={this.state.txtDescription}
+                          onChange={this.onHandleChange}
+                        />
+                      </div>
+
+                      <div className="form-group">
+                        <label>Gender</label>
+                        <select
+                          name="sltGender" 
+                          className="form-control" 
+                          value={this.state.sltGender}
+                          onChange = {this.onHandleChange}
+                        >
+                          <option value={0}>Nữ</option>
+                          <option value={1}>Nam</option>
+                        </select>
+                      </div>
+
+                      <div className="form-group">
+                        <label>Language</label>
+                        <div className="radio">
+                          <label>
+                            <input 
+                              type="radio" 
+                              name="rdLang" 
+                              value="en"
+                              checked={this.state.rdLang === "en"}
+                              onChange = {this.onHandleChange}
+                            />
+                            English 
+                          </label>
+                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                          <label>
+                            <input 
+                              type="radio" 
+                              name="rdLang" 
+                              value="ja"
+                              checked={this.state.rdLang === "ja"}
+                              onChange = {this.onHandleChange}
+                            />
+                            Japanese
+                          </label>
+                        </div>
+                        
+                      </div>
+
+                      <div className="form-group">
+                        <label>Status</label>
+                        <div className="checkbox">
+                          <label>
+                            <input 
+                              type="checkbox" 
+                              name="ckStatus"
+                              value={true}
+                              checked={this.state.ckStatus === true}
+                              onChange = {this.onHandleChange}
+                            />
+                            Married
+                          </label>
+                        </div>
+                        
+                      </div>
+                      
+                      
+                      <br/>
+                      <button type="submit" className="btn btn-primary">Save</button>&nbsp;
+                      <button type="reset" className="btn btn-default">Delete</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            
+            
+            
+
             <div className="row padding-top-50 change_color_font ">
               <ColorPicker color={this.state.color} onReceiveColor = {this.onSetColor} />
               <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6 font_setting">
